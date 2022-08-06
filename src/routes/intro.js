c9b7bce9214db0971b50b4333c3c2721a06c481e
ankit <ANKIT721760@GMAIL.COM>
2022-08-06 12:46:58 +0530

    demo

---
+++ src/routes/intro.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const abc = require('../introduction/intro')
+const router = express.Router();
+router.get('/test-me', function (req, res) {
+    console.log('My batch is', abc.name)
+    abc.printName()
+    res.send('My second ever api!')
+
+});
