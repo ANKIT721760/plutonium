ab25dc4defe006df29ce28c8594b3f6370d55805
ankit <ANKIT721760@GMAIL.COM>
2022-08-09 11:00:33 +0530

    postman

--- src/routes/route.js
+++ src/routes/route.js
@@ -52,7 +52,7 @@ router.get('/movies', function (req, res) {
 
 router.get('/movies/:indexNumber', function (req, res) {
 
-    const movieNames=['Batman Begins','The Dark Knight','The Dark Knight Rises','IronMan']
+    const movieNames=['Rang de basanti','The shining','Lord of the rings','Batman begins']
     if (req.params.indexNumber< movieNames.length)
     {   
     res.send(movieNames[req.params.indexNumber])
@@ -62,16 +62,42 @@ router.get('/movies/:indexNumber', function (req, res) {
     }
 });
 
-//Handle a scenario in problem 2 where if the index is greater than the valid maximum value
-// a message is returned that tells the user to use a valid index in an error message.
+// Write another api called GET /films. Instead of an array of strings define an array of movie objects this time.
+ //Each movie object should have values - id, name. An example of movies array is 
+
 
 router.get('/films', function (req, res) {
 
     const filmNames=[{id:1, name:"Don"},{id:2, name:"RaOne"},{id:3, name:"Swades"},{id:4, name:"Kal Ho Naa Ho"}]
-
-   
     res.send(filmNames)
+
 });
+//Write api GET /films/:filmId where filmId is the value received in request path params.
+// Use this value to return a movie object with this id. In case there is no such movie present in the array, 
+//return a suitable message in the response body. Example for a request GET /films/3 should return the movie object 
+// {
+//     “id”: 3,
+//     “name”: “Rang de Basanti”
+//    }
+//    Similarly for a request GET /films/9 the response can be something like - ‘No movie exists with this id’
+   
+router.get('/films/:filmId', function (req, res) {
+
+    const id= req.params.filmId
+    const filmNames=[{id:1, name:"Don"},{id:2, name:"RaOne"},{id:3, name:"Swades"},{id:4, name:"Kal Ho Naa Ho"}]
+    let arr=[]
+    filmNames.forEach(x => {
+        if (id == x.id){
+            arr=x;
+        }        
+     });
+     if (arr.length !== 0){
+         res.send(arr)
+     } else {
+         res.send('No movie exists with this id')
+     }
+});
+
 
 
 module.exports = router;
