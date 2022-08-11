d16a0bd7c4cf5eca1969082881b371618e392408
ankit <ANKIT721760@GMAIL.COM>
2022-08-11 23:29:50 +0530

    player

---
+++ src/routes/player.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+       //LOGIC WILL COME HERE
+       let addPlayer= req.body
+       let count=0;
+       players.forEach(x => {
+           if(addPlayer.name == x.name){
+            count++
+           }
+       });
+       
+        if(count === 0){
+       players.push(addPlayer)
+       }
+        res.send(  { data: players , status: true }  )
+   })
+   module.exports = router;
+
