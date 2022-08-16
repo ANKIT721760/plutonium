9794e05f167e18abf4e383d4db3993ec645e9e7b
ANKIT721760 <ankit721760@gmail.com>
2022-08-16 06:58:40 -0700

    books

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ankit:ri5zuN60qKfv1Ebi@cluster0.dkoko6x.mongodb.net/ankit813045-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
