ecbfd75d6d2c052e77edfa828732f4e62aa3a317
ANKIT721760 <ankit721760@gmail.com>
2022-08-31 23:34:10 +0530

    try and catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,100 @@
-const UserModel= require("../models/userModel")
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
+//Q.1
+const createUser = async function (req, res) {
+  let data = req.body;
+  try{
+    let savedData = await userModel.create(data);
+  res.status(200).send({ msg: savedData });
+  }catch(error){
+    res.status(400).send({msg:error.message})
+  }
+  
+};
 
+//Q.2
+const loginUser = async function (req, res) {
+  try{
+    let userName = req.body.emailId;
+    let password = req.body.password;
+  
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "Radon",
+        organisation: "FunctionUp",
+      },
+      "functionup-radon"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(201).send({ status: true, token: token });
+  }catch(error){
+    res.status(403).send({msg:error.message});
+  };
+  };
+  
 
+//Q.3
+const getUserData = async function (req, res) {
+  try{
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+    // if (!userDetails) {
+    //   return res.send({ status: false, msg: "No such user exists" });
+    // }
+  
+    res.status(202).send({ status: true, data: userDetails });
+  }catch(error){
+    res.status(403).send({msg:error.message});
+  };
+  };
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
+//Q.4
+const updateUser = async function (req, res) {
+try{
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  //Return an error if no user with the given id exists in the db
+  // if (!user) {
+  //   return res.send("No such user exists");
+  // }
 
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  res.status(202).send({ status: updatedUser, data: updatedUser });
+}catch(error){
+  res.status(401).send({msg:error.message});
+};
+};
+  
 
+//Q.5
+const deleteUser = async function (req, res) {
+ 
+try{
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
 
+  // if(!user){
+  //   return res.send({msg:"No Such User! Try Again.."})
+  // }
+  
+  let userDelete=await userModel.findOneAndUpdate({_id:userId},{"isDeleted":true});
+  res.status(202).send({userDelete})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+}catch(error){
+  res.status(401).send({msg:error})
 }
+  
+};
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
